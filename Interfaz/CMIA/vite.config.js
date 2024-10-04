142db61e809d5742efa8a3c2f79bea0e0574130a
dlegarda <legarda8027@gmail.com>
2024-10-04 00:00:00 -0500

    conf vite

--- Interfaz/CMIA/vite.config.js
+++ Interfaz/CMIA/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Cambia este puerto si es necesario
-    host: '0.0.0.0', // Para permitir el acceso externo
+    port: 5173, 
+    host: '0.0.0.0', 
   },
 })
